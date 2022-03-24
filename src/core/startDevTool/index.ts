5a03d7c4884824064ddc0077777bc811b9d5a37e
voraton.l <voraton@lmwn.com>
2022-03-24 14:09:20 +0700

    fix: Make the devtool work

--- src/core/startDevTool/index.ts
+++ src/core/startDevTool/index.ts
@@ -1,10 +1,21 @@
 import replaceSourceWithTargetSource from 'core/replaceSourceWithTargetSource'
+import { REPLACEMENT_SOURCE_KEY } from 'debugConstants'
 import ShakeDetector from 'shake-detector'
+import isEmpty from 'lodash/isEmpty'
 
 function startDevTool() {
     const shakeDetector = new ShakeDetector({})
 
     shakeDetector.subscribe(() => {
+        const target = prompt('Input your target replacement source url or leave it blank if no replacement wanted')
+        
+        if (!isEmpty(target)) {
+            sessionStorage.setItem(REPLACEMENT_SOURCE_KEY, target!)
+        } else {
+            sessionStorage.removeItem(REPLACEMENT_SOURCE_KEY)
+            location.reload()
+        }
+
         replaceSourceWithTargetSource()
 
         shakeDetector.stop()
