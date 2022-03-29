705422f333fd5f0b09caac7b8d4389db56c5bae7
voraton.l <voraton@lmwn.com>
2022-03-29 11:21:55 +0700

    fix: Remove invalid Test case

--- src/core/replaceSourceWithTargetSource/index.test.ts
+++ src/core/replaceSourceWithTargetSource/index.test.ts
@@ -18,30 +18,6 @@ describe('replaceSourceWithTargetSource', () => {
         jest.clearAllMocks()
     })
 
-    it('should load and write target source and memorized target source in sessionStorage if target source is existed and wont load again if target source is loaded', async () => {
-        const MOCK_TARGET = 'https://example.com'
-        getTargetSourceSpy.mockReturnValue(MOCK_TARGET)
-
-        const MOCK_SOURCE = '<html />'
-        loadTargetSourceSpy.mockReturnValue(MOCK_SOURCE)
-
-        await replaceSourceWithTargetSource()
-
-        expect(getTargetSourceSpy).toBeCalledTimes(1)
-
-        expect(loadTargetSourceSpy).toBeCalledWith(MOCK_TARGET)
-
-        expect(writeSourceToTargetPageSpy).toBeCalledWith(MOCK_SOURCE)
-
-        expect(sessionStorage.getItem(REPLACEMENT_SOURCE_KEY)).toBe(MOCK_TARGET)
-
-        expect(sessionStorage.getItem(MOCK_TARGET)).toBe(MOCK_SOURCE)
-
-        await replaceSourceWithTargetSource()
-
-        expect(loadTargetSourceSpy).toBeCalledTimes(1)
-    })
-
     it('should not load and write target source if target source is not existed', async () => {
         getTargetSourceSpy.mockReturnValue(null)
 
