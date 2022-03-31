95efbdc70af9e6ec4b31487e90229029b7d18df1
voraton.l <voraton@lmwn.com>
2022-03-31 11:24:07 +0700

    fix indent

--- rollup.config.js
+++ rollup.config.js
@@ -9,37 +9,37 @@ import visualizer from 'rollup-plugin-visualizer'
 import config from './package.json'
 
 const sharedPlugins = [
-  resolve({
-    browser: true,
-  }),
-  commonjs(),
-  typescript({
-    declaration: true,
-    declarationDir: config.deploy,
-    exclude: 'src/**/*.test.ts',
-  }),
-  json(),
-  visualizer({
-    filename: path.resolve(config.deploy, 'stat.html'),
-  }),
+	resolve({
+		browser: true,
+	}),
+	commonjs(),
+	typescript({
+		declaration: true,
+		declarationDir: config.deploy,
+		exclude: 'src/**/*.test.ts',
+	}),
+	json(),
+	visualizer({
+		filename: path.resolve(config.deploy, 'stat.html'),
+	}),
 ]
 
 export default [
-  {
-    input: 'src/executors/code-blocker/index.ts',
-    output: {
-      dir: config.deploy,
-      format: 'cjs',
-    },
-    plugins: sharedPlugins
-  },
-  {
-    input: 'src/executors/source-replacement/index.ts',
-    output: {
-      dir: config.deploy,
-      format: 'umd',
-      name: 'SourceReplacement',
-    },
-    plugins: sharedPlugins,
-  },
+	{
+		input: 'src/executors/code-blocker/index.ts',
+		output: {
+			dir: config.deploy,
+			format: 'cjs',
+		},
+		plugins: sharedPlugins
+	},
+	{
+		input: 'src/executors/source-replacement/index.ts',
+		output: {
+			dir: config.deploy,
+			format: 'umd',
+			name: 'SourceReplacement',
+		},
+		plugins: sharedPlugins,
+	},
 ]
